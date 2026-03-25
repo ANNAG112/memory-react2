f4987fdf4d81a62802ae369d2107677b19fe700d
JuliaMuk <mukhina_ur@mail.ru>
2026-03-25 09:11:56 +0500

    routers

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,8 +1,18 @@
+import { Link } from "react-router-dom"
+import images from "../data.json"
+import Card from "./Card/Card"
 export default function List()
 {
     return(
         <>
-            список кратинок
+            <h2>Список картинок</h2>
+            {
+                images.map((item)=>(
+                    <Link to={`/images/item/${item.id}`}>
+                        <Card url={item.url} key={item.id}/>
+                    </Link>
+                ))
+            }s
         </>
     )
 }
